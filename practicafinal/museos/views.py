dd076ff8ee22f8b8244100f1a0a189ec29a76958
dani <danisako92@gmail.com>
2018-05-04 00:15:58 +0200

    Añadido en views.py los filtros y ordenar por comentarios

--- practicafinal/museos/views.py
+++ practicafinal/museos/views.py
@@ -9,6 +9,7 @@ from django.template.loader import get_template
 from django.template import Context
 from django.db.models.functions import Length
 from museos.parse import ParsearMuseos
+from museos import models
 
 
 boton = """
@@ -17,6 +18,27 @@ boton = """
 			<input type="submit" value="Accesibles">
 			</form>
 		"""
+
+form = """
+		<form action = "/" method ="POST">
+			<input type="hidden" name="option" value="1"><br>
+			<input type="submit" value="Cargar">
+			</form>
+		"""
+		
+coment = """
+	<form action = "" method ="POST">
+		Comentario: <input type="text" name="comentario" value=""><br>
+		<input type="submit" value="Enviar">
+		</form>
+"""
+
+filtro = """
+	<form action = "" method ="POST">
+		Filtrar por distrito: <input type="text" name="filtro" value=""><br>
+		<input type="submit" value="Enviar">
+		</form>
+"""
 
 
 
@@ -31,12 +53,6 @@ def barra(request):
 		
 		respuesta = ""
 		respuesta += "ACTUALIZA..</br>"
-		form = """
-		<form action = "/" method ="POST">
-			<input type="hidden" name="option" value="1"><br>
-			<input type="submit" value="Cargar">
-			</form>
-		"""
 		
 			
 
@@ -51,17 +67,19 @@ def barra(request):
 		respuesta = logged
 
 		if logged == True:
-			respuesta += 'Click aquí para favoritos: <a href = "/'+request.user.username+'">Favoritos</a>'
+			respuesta = 'Logged in as --> ' + request.user.username + '<a href="/logout">Logout</a></br> Click aquí para favoritos: <a href = "/'+request.user.username+'">Favoritos</a>'
 		else:
 			respuesta += 'Para poder comentar logeate!  <a href = "/login">Login</a>'
 
 		museos = Museo.objects.all() #devuelve una lista con todos los museos
 		respuesta += "<ul>"
+		###https://stackoverflow.com/questions/9834038/django-order-by-query-set-ascending-and-descending
+		museo = Museo.objects.all().order_by('-comentarios')
 
-		for m in museos:
-			respuesta += "Nombre Museo:  <a href="+m.enlace+">"+m.nombre+"</a></br>" #Muestro la información de los museos que tengo.
+		for m in museo:
+			respuesta += "Nombre Museo:  <a href="+m.enlace+">"+m.nombre+"</a></br> Num: "+str(m.comentarios)+"</br>" #Muestro la información de los museos que tengo.
 			 
-			respuesta+= '<li><a href="'+m.enlace+'"> Mas información </a></br>' #Muestro la página de información para el museo
+			respuesta+= '<li><a href="/museos/'+str(m.id)+'"> Mas información </a></br>' #Muestro la página de información para el museo
 		respuesta+= "</ul>"
 		#print(m.nombre)
 
@@ -71,38 +89,52 @@ def barra(request):
 		for f in favoritos:
 			respuesta += "Museo:  "+ str(m.nombre)+ " </br>" #Muestro la información de los museos que tengo.
 			respuesta += "usuario:  "+ f.usuario+ " </br>" 
-
+		
+		return HttpResponse(form + boton+respuesta)
 
 	elif request.method == "POST":
-		listamuseos = []
-		museos = Museo.objects.all()
-		listamuseos = ParsearMuseos()	
-		for museos in listamuseos:
-	# En cada campo del models metemos lo que hemos parseado
 		
-			museo_new = Museo(nombre=museos["NOMBRE"],
-								accesibilidad=museos["ACCESIBILIDAD"],
-								enlace=museos["CONTENT-URL"],
-								nombrevia=museos["NOMBRE-VIA"],
-								clasevia=museos["CLASE-VIAL"],
-								tiponum=museos["TIPO-NUM"],
-								numero=museos["NUM"],
-								localidad=museos["LOCALIDAD"],
-								provincia=museos["PROVINCIA"],
-								codigopostal=museos["CODIGO-POSTAL"],
-								barrio=museos["BARRIO"],
-								distrito=museos["DISTRITO"],
-								coordx=museos["COORDENADA-X"],
-								coordy=museos["COORDENADA-Y"],)
+		if request.POST['option'] == "1":
+			listamuseos = []
+			museos = Museo.objects.all()
+			listamuseos = ParsearMuseos()	
+			for museos in listamuseos:
+		# En cada campo del models metemos lo que hemos parseado
+	
+				museo_new = Museo(nombre=museos["NOMBRE"],
+									accesibilidad=museos["ACCESIBILIDAD"],
+									enlace=museos["CONTENT-URL"],
+									nombrevia=museos["NOMBRE-VIA"],
+									clasevia=museos["CLASE-VIAL"],
+									tiponum=museos["TIPO-NUM"],
+									numero=museos["NUM"],
+									localidad=museos["LOCALIDAD"],
+									provincia=museos["PROVINCIA"],
+									codigopostal=museos["CODIGO-POSTAL"],
+									barrio=museos["BARRIO"],
+									distrito=museos["DISTRITO"],
+									coordx=museos["COORDENADA-X"],
+									coordy=museos["COORDENADA-Y"],)
+		
+
+				museo_new.save()
 			
+			resp = '<head><meta http-equiv="Refresh" content="	;url='"http://127.0.0.1:8000"'"></head>'" Redirigiendo a la pagina principal "
+			return HttpResponse(resp)
 
-			museo_new.save()
+		elif request.POST['option'] == "2":
 
-		Listado = []
-		print(listamuseos)
-		resp = '<head><meta http-equiv="Refresh" content="1	;url='"http://127.0.0.1:8000"'"></head>'" Redirigiendo a la pagina principal "
-		return HttpResponse(resp)
-	return HttpResponse(respuesta+form + boton)
+			museos = Museo.objects.all()
+			
+			filtro = Museo.objects.filter(accesibilidad = 1)
+			respuesta = "<ul>"
+			respuesta += " "
+			for m in filtro:
+	#			respuesta = "Nombre Museo:  <a href="+">"+filtro.nombre+"</a></br>" #Muestro la información de los museos que tengo.
+				respuesta+= "<li>Nombre: "+m.nombre+ "</br>Accesibilidad:  "+str(m.accesibilidad)+'</br>' #Muestro la página de información para el museo
+			respuesta+= "</ul>"
+		
+	return HttpResponse(boton +form +respuesta)
 
 
 @csrf_exempt
@@ -121,7 +153,59 @@ def usuario(request,nombre):
 
 
 
+@csrf_exempt
+def museos(request):
+	museos = Museo.objects.all()
+	respuesta = "<h2>LISTA CON TODOS LOS MUSEOS</h2><ul>"
+
+	respuesta = filtro
+	if request.method == "GET":
+		for m in museos:
+			respuesta += "Nombre Museo:  "+m.nombre+"</br>" #Muestro la información de los museos que tengo.
+			respuesta+= "Para dar favorito, pulsa: <a href='/favorito'>Favorito</a></br>"	 
+			respuesta+= '<li><a href="'+m.enlace+'">Enlace</a></br></br>' #Muestro la página de información para el museo
+		respuesta+= "</ul>"
+
+	elif request.method == "POST":
+		dist = request.POST['filtro']
+		filtros = Museo.objects.filter(distrito = dist)
+		for m in filtros:
+			respuesta += "<li>Nombre:" +m.nombre + " Distrito: " +m.distrito + "</br>"
+
+	return HttpResponse(respuesta)
+		
+@csrf_exempt
+def paginamuseo(request,identificador):
+
+	museos = Museo.objects.get(id = identificador)
+	com = Comentario.objects.all()
+	respuesta = "<h3>BIENVENIDO A LA PÁGINA DEL MUSEO:</h3></br>"
+	respuesta +="Nombre --> "+ museos.nombre +"</br> Accesibilidad -->"+ str(museos.accesibilidad) +"</br>Distrito --> "+ museos.distrito +"</br>Número Comentarios -->" + str(museos.comentarios)+"</br>"
+	respuesta += "<a href='/'> Volver a la pagina principal</a> "
+	
+	if request.method == "GET":
+		if request.user.is_authenticated():
+			respuesta += 'Logged in as --> ' + request.user.username + '<a href="/logout">Logout</a></br>'
+		
+			respuesta += coment
+
+		else:
+			respuesta += 'Not logged in. <a href = "/login">Login</a></br>'
+	
+
+	#return HttpResponse(respuesta)
+
+	elif request.method == "POST":
+		comentario = Comentario(contenido = request.POST.get('comentario'), museo = museos)
+		museos.comentarios = museos.comentarios + 1
+		museos.save()
+		comentario.save()
+
+		respuesta += "Comentario añadido!"
 
 
+		
+
+	return HttpResponse(respuesta)
 
 
